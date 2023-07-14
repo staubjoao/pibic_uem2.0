ce2c6cdfa796f29c8b723018ae0880ed4478ede5
João Staub <joao.staub42@gmail.com>
2023-07-14 14:05:56 -0300

    testar

--- testar_pc_din_fusion.py
+++ testar_pc_din_fusion.py
@@ -213,11 +213,13 @@ for train, test in kfold.split(images, labels):
     svm_model = SVC(C=100, kernel='poly', gamma='scale')
     svm_model.fit(x_train_svm, y_train_svm)
     svm_predictions = svm_model.predict(x_test_svm)
+    print("SVM: ", svm_predictions.shape())
 
     # CNN
     scores = model.evaluate(x_test_cnn, y_test_cnn, verbose=0)
     histories.append(model.history.history)
     cnn_predictions = model.predict(x_test_cnn)
+    print("CNN: ", cnn_predictions.shape())
 
     results = {}
 
